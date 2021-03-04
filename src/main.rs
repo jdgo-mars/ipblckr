6337494b795e5de5859be40205840f71b1233166
Joao Martins <mdj@amazon.com>
2021-03-04 13:28:05 +0200

    add some initial config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate dotenv_codegen;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::{env, fs, path::Path};
+
+use pickledb::{PickleDb, PickleDbDumpPolicy};
+
+const DATA_PATH: &str = dotenv!("DATA_PATH");
+const LOGS_PATH: &str = dotenv!("LOGS_PATH");
+
+fn ensure_path(p: &str) {
+    let p = Path::new(p);
+    if !p.exists() {
+        fs::create_dir_all(p).unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    ensure_path(DATA_PATH);
+    ensure_path(LOGS_PATH);
+
+    let mut db = PickleDb::new_json(dotenv!("DB_NAME"), PickleDbDumpPolicy::AutoDump);
 }
